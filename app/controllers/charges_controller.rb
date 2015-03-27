5599564671219f65c1b00e9a84768f951e636eeb
NL33 <stkellman@gmail.com>
2015-03-27 10:26:32 -0400

    accept credit cards through stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,25 +3,24 @@ class ChargesController < ApplicationController
  end
 
  def create
-  # Amount in cents
-  @amount = 500 #this is what the charge will be.
+    # Set your secret key: remember to change this to your live secret key in production
+    # See your keys here https://dashboard.stripe.com/account/apikeys
+    Stripe.api_key = "sk_test_IijRDLY7kClbd5DiyCNmwLQy"
 
-  customer = Stripe::Customer.create(
-    :email => params[:stripeEmail], #this is the customer email
-    :card  => params[:stripeToken] 
-  )
+    # Get the credit card details submitted by the form
+    token = params[:stripeToken]
 
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Rails Stripe customer',
-    :currency    => 'usd'
-  )
-
-  rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to charges_path
-
- end
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        :amount => 1000, # amount in cents, again
+        :currency => "usd",
+        :source => token,
+        :description => "payinguser@example.com"
+      )
+    rescue Stripe::CardError => e
+       The card has been declined
+    end
+  end
 
 end
