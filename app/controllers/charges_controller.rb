2b36a6096873e66f2d9ce94cde615e84a3e2e4e0
NL33 <stkellman@gmail.com>
2015-03-27 14:00:41 -0400

    add functionality for charging the credit card

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,22 +5,30 @@ class ChargesController < ApplicationController
  def create
     # Set your secret key: remember to change this to your live secret key in production
     # See your keys here https://dashboard.stripe.com/account/apikeys
-    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+    #Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
     # Get the credit card details submitted by the form
     token = params[:stripeToken]
 
+    @entered_amount = params[:amount]
+    no_commas = @entered_amount.gsub(/,/,'').to_i 
+    amount = (no_commas * 100)
+
     # Create the charge on Stripe's servers - this will charge the user's card
+   if amount > 0
     begin
       charge = Stripe::Charge.create(
-        :amount => 1000, # amount in cents, again
+        :amount => amount, # amount in cents, again
         :currency => "usd",
         :source => token,
-        :description => "payinguser@example.com"
+        :description => params[:email]
       )
     rescue Stripe::CardError => e
-       The card has been declined
     end
+  else 
+    flash[:notice] = "You entered an invalid amount. Please try again" #flash not showing up for some reason
+    redirect_to root_path
   end
+ end
 
 end
